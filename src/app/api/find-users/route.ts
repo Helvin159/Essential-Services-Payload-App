011a755fc37646400c4c521a85bcb0dbf1065c3e
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2024-11-21 21:20:54 -0500

    find users api route

---
+++ src/app/api/find-users/route.ts
@@ -0,0 +1,23 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    const payload = await getPayload({ config })
+
+    const result = await payload
+      .find({
+        collection: 'users',
+        limit: 0,
+        sort: 'fullName',
+      })
+      .then((data) => data.docs)
+
+    NextResponse.json(result, { status: 200 })
+  } catch (e) {
+    console.error(e)
+
+    return NextResponse.json('Error fetching data', { status: 500 })
+  }
+}
